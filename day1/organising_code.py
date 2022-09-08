3b1212a36f12beaab6fd9a952d393033ec9bffcf
paulkorir <paul@sci2pro.com>
2022-09-08 05:55:44 +0100

    completed

--- day1/organising_code.py
+++ day1/organising_code.py
@@ -1,12 +1,35 @@
 import sys
 
-import exceptions
+
+def importing_modules():
+    import exceptions
+
+    a = int(input("a: "))
+    r = float(input("r: "))
+    n = int(input("n: "))
+    s_n = exceptions.calculate_geometric_series(a, r, n)
+    print(f"s_n = {s_n}")
+
+
+def importing_packages():
+    n = int(input("n: "))
+
+    import pkg1
+    print(f"L_{n} = {pkg1.my_module.calculate_lucas_numbers(n)}")
+
+    import pkg1 as lucas_package
+    print(f"L_{n} = {lucas_package.my_module.calculate_lucas_numbers(n)}")
+
+    from pkg1 import my_module
+    print(f"L_{n} = {my_module.calculate_lucas_numbers(n)}")
+
+    from pkg1 import my_module as lucas_module
+    print(f"L_{n} = {lucas_module.calculate_lucas_numbers(n)}")
 
 
 def main():
-    print(
-        f"""{exceptions.calculate_geometric_series(int(input("a: ")), float(input("r: ")), int(input("n: "))) = }"""
-    )
+    # importing_modules()
+    importing_packages()
     return 0
 
 
