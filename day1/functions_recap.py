116d6089c08b2e68bcfe75684f638b7c39e0a2a8
stefanusbernard <stefanus.bernard@gmail.com>
2022-09-19 11:42:09 +0100

    create function max

--- day1/functions_recap.py
+++ day1/functions_recap.py
@@ -9,11 +9,25 @@ def calculate_geometric_series(a, r, n=10):
 
 
 def main():
-    a = int(input("a: "))
-    r = float(input("r: "))
-    n = int(input("n: "))
-    print(f"s_n = {calculate_geometric_series(a, r, n)}")
+    # a = int(input("a: "))
+    # r = float(input("r: "))
+    # n = int(input("n: "))
+    # print(f"s_n = {calculate_geometric_series(a, r, n)}")
+    my_list = [1, 7, 9, 42]
+    my_max = get_max(my_list)
+    print(f"{my_max = }")
     return 0
+
+def get_max(a_list):
+    maximum = None
+    first = False
+    for value in a_list:
+        if not first:
+            maximum = value
+            first = True
+        if value > maximum:
+            maximum = value
+    return maximum
 
 
 if __name__ == "__main__":
